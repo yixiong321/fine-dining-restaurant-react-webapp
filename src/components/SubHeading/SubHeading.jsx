192ac1ffca6f82ad30ff8b6508f58ecddf940221
yixiong321 <yixiong321@gmail.com>
2023-07-13 16:03:33 +0800

    first

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+import { images } from '../../constants';
+const SubHeading = (props) => (
+  <div style={{marginBottom: '1rem'}}>
+    <p className="p__cormorant">{props.title}</p>
+    <img src={images.spoon} alt="spoon" className="spoon__img"></img>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import images from "../../constants/images"
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div id="home" className="app__header app__wrapper section __padding">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new Flavour"></SubHeading>
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>  
+      <p className="p__opensans" style={{margin:'2rem 0'}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni quis voluptatum nisi libero molestiae quibusdam pariatur totam ab. Praesentium ex corporis quo! Quis aspernatur excepturi esse impedit, maiores ipsa?</p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header-img"/>
+    </div>
   </div>
 );
 
